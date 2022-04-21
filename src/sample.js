02dd90880fe7ba431fdd88c542627562dc7f21de
screw24 <screw24@nate.com>
2022-04-21 13:33:20 +0900

    수정사항

---
+++ src/sample.js
@@ -0,0 +1,8 @@
+let numbers = [10, -5, 2, -4]
+// let filtered = []
+// for(var i = 0; i < numbers.length; i++) {
+//     if(numbers[i] > 0) {
+//         filtered.push(numbers[i])
+//     }
+// }
+console.log(numbers.filter(grade => grade > 0))
