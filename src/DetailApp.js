2b5b81ba4bff8039571a1e47d1a71080b03427a9
screw24 <screw24@nate.com>
2022-05-19 15:56:57 +0900

    수정

--- src/DetailApp.js
+++ src/DetailApp.js
@@ -4,7 +4,7 @@ function DetailApp(props) {
     return (
         <div style={{backgroundColor:'blue', display:'flex', flexDirection:'row', flex: '1 1 0', overflow:'hidden'}}>
               <div style={{backgroundColor:'green', display:'flex', flex: '1 0 0', flexDirection:'column', overflowY:'auto'}}>
-                <h2>siasiaai</h2>
+                <h2>날아가자</h2>
                 <div style={{backgroundColor:'yellow', display:'flex', flex: '1 0 0', flexDirection:'column', overflowY:'auto', padding:20}}>
                   555<br />555<br />5<br />555<br />555<br />555<br />555<br />5<br />555<br />555<br />555<br />555<br />555<br />5<br />555<br />555<br />555<br />555<br />555<br />5<br />555<br />555<br />
                 </div></div>
